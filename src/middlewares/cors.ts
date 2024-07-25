5257ecf49bee7e663005ddcc37c5ed214bc501bd
Carlos <carlosmiguelgarcesbasulto@gmail.com>
2024-07-25 05:19:38 -0400

    cors

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,19 +1,23 @@
 import cors from "cors";
 
+const Accepted_Origins = [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    "https://d-kanto-admin.netlify.app",
+    "https://d-kanto-frontend.netlify.app",
+]
+
 export const corsMiddleware = () => cors({
     origin: (origin, callback) => {
-        const Accepted_Origins = [
-            "http://localhost:5173",
-            "http://localhost:4173",
-            "https://d-kanto-admin.netlify.app",
-            "https://d-kanto-frontend.netlify.app",
-        ]
-
         if (Accepted_Origins.includes(origin) || !origin) {
             return callback(null, true);
         }
 
+        console.log(Accepted_Origins.includes(origin));
+        
         console.log(origin);
+        
+
         return callback(new Error("Not allowed by CORS"));
     },
     credentials: true
