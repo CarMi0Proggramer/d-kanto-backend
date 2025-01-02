cfb5feae8ac2cf1b6b66be92b98ec09d92bd2e15
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-01-02 04:27:56 -0500

    Database updated

--- src/app-data-source.ts
+++ src/app-data-source.ts
@@ -1,20 +1,11 @@
-import { DataSource } from "typeorm"
-
-const host = "roundhouse.proxy.rlwy.net"
-const port = 48361
-const password = "xYVbmPTsDmpBMXBekgtUKxrwiVWiepOI"
-const username = "postgres"
-const database = "railway"
+import { DataSource } from "typeorm";
 
 export const myDataSource = new DataSource({
     type: "postgres",
-    host: host,
-    port: port,
-    username: username,
-    password: password,
-    database: database,
+    url: process.env.DATABASE_URL,
+    ssl: true,
     entities: ["dist/entity/*.js"],
     migrations: [],
     logging: true,
-    synchronize: true
-})
+    synchronize: true,
+});
