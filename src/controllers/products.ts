3c3971f27bec7a50c8b422afa776cb3dca8a293c
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-01-02 04:41:47 -0500

    Get All products method updated

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,48 +1,46 @@
-
-import { Request, Response } from "express"
+import { Request, Response } from "express";
 import { ProductModel } from "../models/product";
-import { validatePartialProduct, validateProduct } from "../schemas/products.schema";
+import {
+    validatePartialProduct,
+    validateProduct,
+} from "../schemas/products.schema";
 
-export class ProductController  {
-    static async getAll(req: Request, res: Response){
+export class ProductController {
+    static async getAll(req: Request, res: Response) {
         try {
             const products = await ProductModel.getAll();
-            if (products.length === 0) {
-                res.status(404).json({message: "No products found"});
-            }else{
-                res.status(200).json(products);
-            }
+            res.json(products);
         } catch (error) {
-            res.status(500).json({message: "Internal Server error"});
+            res.status(500).json({ message: "Internal Server error" });
         }
     }
 
-    static async getById( req: Request, res: Response){
+    static async getById(req: Request, res: Response) {
         let { id } = req.params;
         try {
             const product = await ProductModel.getById(Number(id));
             if (!product) {
-                res.status(404).json({message: "Product doesn't exist"});
-            }else{
+                res.status(404).json({ message: "Product doesn't exist" });
+            } else {
                 res.status(200).json(product);
             }
-        }catch(error){
-            res.status(500).json({message: "Internal server error"});
+        } catch (error) {
+            res.status(500).json({ message: "Internal server error" });
         }
     }
 
-    static async create(req:Request, res: Response) {
+    static async create(req: Request, res: Response) {
         const data = validateProduct(req.body);
         if (!data.name) {
-            return res.status(400).json({message: data});
+            return res.status(400).json({ message: data });
         }
         try {
             const product = await ProductModel.create(data);
             res.status(201).json(product);
-        }catch(error){
+        } catch (error) {
             res.status(500).json({
-                message: "Internal server error"
-            })
+                message: "Internal server error",
+            });
         }
     }
 
@@ -51,31 +49,33 @@ export class ProductController  {
         try {
             const result = await ProductModel.delete(Number(id));
             if (result.affected === 0) {
-                res.status(404).json({message: "Product doesn't exist"});
-            }else{
+                res.status(404).json({ message: "Product doesn't exist" });
+            } else {
                 res.status(204).send();
             }
-        }catch(error){
-            res.status(500).json({message: "Internal server error"});
+        } catch (error) {
+            res.status(500).json({ message: "Internal server error" });
         }
     }
 
-    static async update( req: Request, res: Response){
+    static async update(req: Request, res: Response) {
         const data = validatePartialProduct(req.body);
         if (!data.name) {
-            return res.status(400).json({message: data})
+            return res.status(400).json({ message: data });
         }
 
         let { id } = req.params;
-    
+
         try {
             const product = await ProductModel.update(Number(id), data);
             if (!product) {
-                return res.status(404).json({message: "Product doesn't exist"});
+                return res
+                    .status(404)
+                    .json({ message: "Product doesn't exist" });
             }
             res.status(200).json(product);
-        }catch(error){
-            res.status(500).json({message: "Internal Server error"});
+        } catch (error) {
+            res.status(500).json({ message: "Internal Server error" });
         }
     }
 }
