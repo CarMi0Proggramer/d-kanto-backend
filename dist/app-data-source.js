8ab6585fdaa2720c549ffe79c314ccae808bac0b
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-01-02 04:36:24 -0500

    Application built

--- dist/app-data-source.js
+++ dist/app-data-source.js
@@ -2,21 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.myDataSource = void 0;
 const typeorm_1 = require("typeorm");
-const host = "roundhouse.proxy.rlwy.net";
-const port = 48361;
-const password = "xYVbmPTsDmpBMXBekgtUKxrwiVWiepOI";
-const username = "postgres";
-const database = "railway";
 exports.myDataSource = new typeorm_1.DataSource({
     type: "postgres",
-    host: host,
-    port: port,
-    username: username,
-    password: password,
-    database: database,
+    url: process.env.DATABASE_URL,
+    ssl: true,
     entities: ["dist/entity/*.js"],
     migrations: [],
     logging: true,
-    synchronize: true
+    synchronize: true,
 });
 //# sourceMappingURL=app-data-source.js.map
